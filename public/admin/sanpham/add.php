e239988ef512c095bebc7f019c27eb82b026fc3f
huynh23trh <118797841+huynh23trh@users.noreply.github.com>
2022-12-01 17:04:23 +0700

    [admin]themsanpham

--- public/admin/sanpham/add.php
+++ public/admin/sanpham/add.php
@@ -1,1 +1,48 @@
-<h1>thêm sản phẩm</h1>
+<div class="row">
+    <div class="mb10 titlead">
+        <h2>THÊM MỚI SẢN PHẨM</h2>
+    </div>
+
+    <div class="row  form-admin">
+        <form action="index.php?act=addsp" method="post">
+            <div class="row mb10">
+                Mã sản phẩm <br>
+                <input type="text" name="masp" id="" disabled>
+            </div>
+            <div class="row mb10">
+                Tên sản phẩm <br>
+                <input type="text" name="tensp" id="">
+            </div>
+            <div class="row mb10">
+                Giá <br>
+                <input type="number" name="gia" id="">
+            </div>
+            <div class="row mb10">
+                Hình ảnh <br>
+                <input type="file" name="hinhanh" id="">
+            </div>
+            <div class="row mb10">
+                Mô tả <br>
+                <input type="text" name="mota" id="">
+            </div>
+            <div class="row mb10">
+                Mã loại <br>
+                <input type="text" name="maloai" id="">
+            </div>
+            <div class="row mb10">
+                <input type="submit" value="Thêm mới" name="themmoi">
+                <input type="reset" value="Nhập lại">
+
+                <a href="index.php?act=list"><input type="button" value="Danh sách"></a>
+            </div>
+    <?php
+    if(isset($thongbao) && ($thongbao!="")){
+        echo $thongbao;
+    }
+       
+    ?>
+
+        </form>
+    </div>
+</div>
+</div>
