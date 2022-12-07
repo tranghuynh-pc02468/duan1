7860184b12075b4b34d89776ebc0ed81e1a55a7c
huynh23trh <118797841+huynh23trh@users.noreply.github.com>
2022-12-08 02:43:08 +0700

    [upload] viết sql cho dm

--- public/model/function_danhmuc.php
+++ public/model/function_danhmuc.php
@@ -1,1 +1,28 @@
+<?php
+    function add_dm($tenloai){
+        $sql = "INSERT INTO loaihang(tenloai) VALUES('$tenloai')";
+        pdo_execute($sql);
+    }
+
+    function listall_dm(){
+        $sql="SELECT * FROM loaihang order by maloai";
+        $listdm=pdo_query($sql);
+        return $listdm;
+    }
+
+    function listone_dm($maloai){
+        $sql = "SELECT * FROM loaihang where maloai= ".$maloai;
+        $updatedm = pdo_query_one($sql);
         return $updatedm;
+    }
+
+    function delete_dm($maloai){
+        $sql="delete FROM loaihang where maloai= ".$maloai;
+        pdo_execute($sql);
+    }
+
+    function update_dm($maloai,$tenloai){
+        $sql = "UPDATE loaihang SET tenloai='".$tenloai."' WHERE maloai=" .$maloai;
+        pdo_execute($sql);
+    }
+?>
