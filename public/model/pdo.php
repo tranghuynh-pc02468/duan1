51e3285af7d6f41ce60a70817c290d8a47c48bc8
huynh23trh <118797841+huynh23trh@users.noreply.github.com>
2022-12-08 16:18:17 +0700

    [pdo]

--- public/model/pdo.php
+++ public/model/pdo.php
@@ -5,8 +5,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $host_name = "127.0.0.1:3307
-    ";
+    $host_name = "127.0.0.1:3307";
     $db_name = "sneaker_store";
 
     $dburl = "mysql:host=$host_name;dbname=$db_name;charset=utf8";
